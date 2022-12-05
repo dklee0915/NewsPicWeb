6defba75be9eb5d8c6c34b7a3ef9be127dbc5c57
dklee0915 <dklee1127@gmail.com>
2022-12-06 01:17:17 +0900

    Initial commit

---
+++ src/main/java/net/dalnim/NewsPicWeb/NewsPicWebApplication.java
@@ -0,0 +1,13 @@
+package net.dalnim.NewsPicWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPicWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPicWebApplication.class, args);
+	}
+
+}
